567d5c390013a337adbed967b0d005afa27a7796
uvharode <uvharode5@gmail.com>
2021-01-03 21:44:31 +0530

    php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+
+    function display()
+    {
+        $user = User::all();
+        return response()->json($user);
+ 
+    }
+
+    function login(Request $request)
+    {
+        $credentials = $request->only(['email','password']);
+        $user= User::where('email',$request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) 
+        {
+            return response() ->json('invalid  Credential');
+        }
+        return response()->json($user->createToken('auth')->plainTextToken);
+    }
+
+    function register(Request $request)
+    {
+    $credentials = $request->all();
+    $credentials['password'] = Hash::make($credentials['password']);
+    $user = User::create($credentials);
+    //auth()->login($user);
+    return response()->json($user);
+    }
+
+    // function logout($id){
+    //     User::find($id)->delete();
+    // }
+}
